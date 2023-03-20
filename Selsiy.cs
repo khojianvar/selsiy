51ff9516215994b5a11afa5c16ce65402ff77eb2
khojianvar <128412774+khojianvar@users.noreply.github.com>
2023-03-21 01:04:06 +0500

    Add files via upload

---
+++ Selsiy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selsiy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Selsiy: ");
+            double selsiy = double.Parse(Console.ReadLine());
+            double farengeyt = selsiy * 33.8;
+            double kelvin = selsiy * 274.15;
+            Console.WriteLine($"{selsiy}-gradus Selsiy = {farengeyt}-farengeytga");
+            Console.WriteLine($"{selsiy}-gradus Selsiy = {kelvin}-kelvinga");
+            Main(args);
+        }
+    }
+}
